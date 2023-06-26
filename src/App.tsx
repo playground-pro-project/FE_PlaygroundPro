3f546ade1ed041e6d49a9f9221a59be90607bbd1
setyoaldi <achmadsetyoaldi@gmail.com>
2023-06-26 18:23:58 +0700

    style : styling mytransaction and history card

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Validate from "./pages/Validate";
 import DetailVenue from "./pages/DetailVenue";
 import MyVenue from "./pages/MyVenue";
 import AddVenue from "./pages/AddVenue";
+import MyTransaction from "./pages/MyTransaction";
 
 function App() {
   return (
@@ -19,10 +20,11 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/otp" element={<OtpPage />} />
           <Route path="/confirmpay" element={<ConfirmPay />} />
+          <Route path="/mytransaction" element={<MyTransaction />} />
           <Route path="/validate" element={<Validate />} />
           <Route path="/detail" element={<DetailVenue />} />
           <Route path="/myvenue" element={<MyVenue />} />
-          <Route path="/addvenue" element={<AddVenue/>} />
+          <Route path="/addvenue" element={<AddVenue />} />
         </Routes>
       </BrowserRouter>
     </>
